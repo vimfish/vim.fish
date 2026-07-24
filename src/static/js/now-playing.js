1010e7fbf63fc1b0ac81d82372d3263efba44feb
vim <hi@vim.fish>
2026-07-24 20:45:02 -0300

    feat: add latest libre.fm song to homepage

---
+++ src/static/js/now-playing.js
@@ -0,0 +1,17 @@
+async function getLibreFmNowPlaying() {
+  const librefm = await fetch(
+    "https://libre.fm/2.0/?method=user.getrecenttracks&user=vimtriloquist&format=json",
+  ).then((x) => x.json());
+
+  console.log(librefm.recenttracks.track);
+  const artist = librefm.recenttracks.track[0].artist["#text"];
+  const song = librefm.recenttracks.track[0].name;
+
+  const element = document.querySelector("#librefm");
+
+  if (element) {
+    element.innerText = `${artist.toLowerCase()} - ${song.toLowerCase()}`;
+  }
+}
+
+getLibreFmNowPlaying();
